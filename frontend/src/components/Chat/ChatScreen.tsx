ff114c78529d77ed79a1e1b7fac1300073882527
osubasi1 <osmansubasi307@gmail.com>
2021-03-30 21:18:10 -0400

    Updated scroll down

--- frontend/src/components/Chat/ChatScreen.tsx
+++ frontend/src/components/Chat/ChatScreen.tsx
@@ -1,196 +1,107 @@
-import React, {useCallback, useEffect, useRef, useState, createRef} from 'react';
-import axios from 'axios';
-import Client from 'twilio-chat';
-import {Channel} from 'twilio-chat/lib/channel';
-import {Box, Button, Input, Stack, Table, Tbody, Td, Tr, Grid, GridItem} from "@chakra-ui/react";
+import React, { createRef, useCallback, useEffect, useState } from 'react';
+import { Channel } from 'twilio-chat/lib/channel';
+import { Box, Button, Input, Stack } from "@chakra-ui/react";
+import { Message } from 'twilio-chat/lib/message';
+import Video from "../../classes/Video/Video";
 
-import useCoveyAppState from "../../hooks/useCoveyAppState";
-
-type Message = {
-  state: {
-    author: string
-    body: string
-    sid: string
-  }
-};
-
-
-
-export default function ChatScreen(): JSX.Element {
+/**
+ * ChatScreen is a React Component that handles the messaging functionality of a given channel.
+ * @param channel The channel for this ChatScreen.
+ * @returns React component that displays messages, allows input to send messages, and send button.
+ */
+export default function ChatScreen({channel}: { channel: Channel }): JSX.Element {
   const [text, setText] = useState<string>('');
   const [messages, setMessages] = useState<Message[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
-  const [channel, setChannel] = useState<Channel>();
+  const [thisChannel] = useState<Channel>(channel);
 
-  const { currentTownID, currentTownFriendlyName, userName } = useCoveyAppState()
-  const messagesEndRef = useRef(null);
   const chatContainer = createRef<HTMLDivElement>();
 
 
-  // TODO We should probably create a client to communicate to the Chat Backend
-  const getToken = async (email: string) => {
-    const response = await axios.get(`http://localhost:3003/token/${email}`);
-    const { data } = response;
-    return data.token;
-  };
+  // useEffect for message added listener
+  useEffect(()=>{
+    const messageListener =()=>{
+      thisChannel.on("messageAdded", (messageToAdd: Message) => {
+        setMessages(old => [...old, messageToAdd]);
+
+      });
+    };
+    messageListener();
+    return (() => {});
+  }, [thisChannel]);
 
 
-
-
-  const handleMessageAdded = (messageToAdd: Message) => {
-    const message :Message = {
-      state: {
-        author: messageToAdd.state.author,
-        body: messageToAdd.state.body,
-        sid: messageToAdd.state.sid,
-      },
-    }
-    // setMessages(messages ? [...messages, message] : [message])
-    setMessages(oldMessages => [...oldMessages, message]);
-    console.log('messages', messages)
-    console.log('message', message)
-    // setMessages(messages ? [...messages, messageToAdd] : [messageToAdd])
-    // messages.push(messageToAdd)
-    console.log(messages)
-
-  }
-
-  const joinChannel = useCallback(async (channelToJoin) => {
-    if (channelToJoin.channelState.status !== "joined") {
-      await channelToJoin.join();
-    }
-    console.log('messages in joinChannel', messages)
-    channelToJoin.on("messageAdded", handleMessageAdded);
-
-  },[handleMessageAdded, messages]);
-
-/*
-Reference: https://stackoverflow.com/questions/37620694/how-to-scroll-to-bottom-in-react
- */
-  const scrollToBottom = () => {
-    chatContainer.current?.scrollIntoView({ behavior: "smooth" });
-  };
-
-
+  // useEffect for initializing old messages to chatbox
   useEffect(() => {
-    scrollToBottom();
-  }, [messages])
+    let isMounted = true;
+    const handleChannel = async () => {
+      const previousMessages = await thisChannel.getMessages();
+      const mes: Message[] = previousMessages.items;
+      if (isMounted) setMessages(mes);
+    };
+    handleChannel();
+    return () => {
+      isMounted = false
+    };
+  }, [thisChannel]);
 
 
-  const sendMessage =() => {
-    console.log(messages)
+  // sends messages to channel
+  const sendMessage = () => {
     if (text && String(text).trim()) {
-      setLoading(true)
-      // channel && channel.sendMessage(text);
-
-      if (channel) {
-        console.log(text)
-        channel.sendMessage(text)
-      }
-
+      setLoading(true);
+      channel.sendMessage(text);
       setText("");
       setLoading(false);
     }
+  };
 
-  }
 
-  const loginToChat = useCallback(async () => {
+  const scrollToBottom = useCallback (() => {
+    if(chatContainer.current) {
+      const scroll =
+        chatContainer.current.scrollHeight -
+        chatContainer.current.clientHeight;
+      chatContainer.current.scrollTo(0, scroll);
+    }
+  },[chatContainer]);
 
-    let token = '';
-    setLoading(true)
+  useEffect(() => {
+    scrollToBottom();
+    return (() => {});
+  }, [messages, scrollToBottom]);
 
+
+  const getMessageAuthor = (author: string) => {
     try {
-      token = await getToken(userName);
+      return JSON.parse(author).userName
     } catch {
-      throw new Error("unable to get token, please reload this page");
+      return author
     }
-
-    const client = await Client.create(token);
-
-    client.on("tokenAboutToExpire", async () => {
-      const token1 = await getToken(userName);
-      await client.updateToken(token1);
-    });
-
-    client.on("tokenExpired", async () => {
-      const token2 = await getToken(userName);
-      await client.updateToken(token2);
-    });
-
-    client.on("channelJoined", async (channelToJoin) => {
-      // getting list of all messages since this is an existing channel
-      const mes = await channelToJoin.getMessages();
-
-
-      const mes2 : Message[] = mes.items.map((message: Message) => ({
-        state: {
-          body: message.state.body,
-          sid: message.state.sid,
-          author: message.state.author
-        }
-      }))
-      setMessages(mes2)
-
-    });
-
-    try {
-      const channelToJoin = await client.getChannelByUniqueName(currentTownID);
-      joinChannel(channelToJoin).then(()=>{
-        channelToJoin.sendMessage('HAS JOINED THE CHAT')
-      })
-      setChannel(channelToJoin)
-    } catch {
-    try {
-      const channelToJoin = await client.createChannel({
-        uniqueName: currentTownID,
-        friendlyName: currentTownFriendlyName,
-      });
-      joinChannel(channelToJoin).then(()=>{
-        channelToJoin.sendMessage(' HAS ENTERED THE CHAT');
-      });
-      setChannel(channelToJoin)
-      setLoading(false)
-
-      console.log('channel', channel);
-      console.log('messages', messages);
-    } catch {
-      throw new Error('unable to create channel, please reload this page');
-    }
-    }
-  }, [channel, currentTownFriendlyName, currentTownID, joinChannel, messages, userName])
-
-
-  const enterToSend = (event: React.KeyboardEvent<HTMLDivElement>) => {
-    if(event.key === 'Enter'){
-      sendMessage();
-    }
-  }
+  };
 
   return (
     <>
       <Stack>
         <div>
-          <Box maxH="500px" overflowY="scroll">
+          <Box maxH="500px" overflowY="scroll" ref={chatContainer}>
             {messages.map((message) =>
-          <div key={message.state.sid}>
-            <b>{message.state.author}</b>:{message.state.body}
-            <div ref={chatContainer}/>
-          </div>)
+              <div key={message.sid}>
+                <b>{getMessageAuthor(message.author)}</b>:{message.body}
+              </div>)
             }
           </Box>
-        </div>
-        <div>
-        <Input w="90%"
-             autoFocus name="name"
-             placeholder="Type a message"
-             onChange={(event) => setText(event.target.value)}
-             value={text}
-             onKeyDown={(e) => enterToSend(e)}
-        />
-        <div>
+          <Input w="90%" autoFocus name="name" placeholder=""
+                 autoComplete="off"
+                 onChange={(event) => setText(event.target.value)}
+                 value={text}
+                 onKeyPress={event => {
+                   if (event.key === "Enter") sendMessage()
+                 }}
+                 onFocus={() => Video.instance()?.pauseGame()}
+                 onBlur={() => Video.instance()?.unPauseGame()}
+          />
           <Button w="10%" onClick={sendMessage} disabled={!channel || !text}>Send</Button>
-        </div>
-        <Button onClick={loginToChat} >Login to Chat</Button>
         </div>
       </Stack>
     </>
@@ -198,4 +109,3 @@ Reference: https://stackoverflow.com/questions/37620694/how-to-scroll-to-bottom-
 
 
 }
-
