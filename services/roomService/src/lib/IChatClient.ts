05872e03d4de34eb74e7876180d6c14064f7a1e3
calvinchancan <chan.calv@husky.neu.edu>
2021-04-07 17:10:36 -0400

    Removed deleteChannel from the interface and updated documention and changed "video" to "chat".

--- services/roomService/src/lib/IChatClient.ts
+++ services/roomService/src/lib/IChatClient.ts
@@ -1,16 +1,14 @@
 /**
- * The video calling component of Covey.Town must implement this server interface,
- * which is used to authorize a client to connect to a video room.
+ * The chat calling component of Covey.Town must implement this server interface,
+ * which is used to authorize a client to connect to a chat channels.
  */
 export default interface IChatClient {
   /**
-   * Issue a secret token on behalf of the video service that the client will be able to use
-   * to connect to the video room specified.
+   * Issue a secret token on behalf of the chat service that to connect the client to our chat service
    *
-   * @param playerID
-   * @param userName
+   * @param playerID the covey town generated playerID for a specific player
+   * @param userName the username of the player when they entered a town
    */
   getToken(playerID: string, userName: string): Promise<string>;
 
-  deleteChannel(channelSID: string): Promise<boolean>;
 }
