2e33a3dd4d2954d9969fb736f675800252c2a441
calvinchancan <chan.calv@husky.neu.edu>
2021-04-08 18:35:30 -0400

    Added better mock tests and fix some bugs found.

--- services/roomService/src/client/CoveyTownsREST.test.ts
+++ services/roomService/src/client/CoveyTownsREST.test.ts
@@ -5,9 +5,37 @@ import {nanoid} from 'nanoid';
 import assert from 'assert';
 import {AddressInfo} from 'net';
 
-import {mock} from 'jest-mock-extended';
 import TownsServiceClient, {TownListResponse} from './TownsServiceClient';
 import addTownRoutes from '../router/towns';
+import TwilioChat from '../lib/TwilioChat';
+
+jest.mock('../lib/TwilioChat');
+
+const mockGetChatTokenForTown = jest.fn();
+const mockCreateChannelWithBot = jest.fn();
+const mockCreateChannel = jest.fn();
+const mockSendInvite = jest.fn();
+const mockUpdateChannel = jest.fn();
+const mockGetChannels = jest.fn();
+const mockDeleteChannel = jest.fn();
+
+// eslint-disable-next-line
+// @ts-ignore it's a mock
+TwilioChat.getInstance = () => ({
+  getToken: mockGetChatTokenForTown,
+  createChannelWithBot: mockCreateChannelWithBot,
+  createChannel: mockCreateChannel,
+  sendInvite: mockSendInvite,
+  deleteChannel: mockDeleteChannel,
+  updateChannel: mockUpdateChannel,
+  getChannels: mockGetChannels,
+});
+
+mockCreateChannel.mockReturnValue(Promise.resolve({
+  sid: nanoid(),
+}));
+
+mockGetChatTokenForTown.mockReturnValue(Promise.resolve(nanoid()));
 
 type TestTownData = {
   friendlyName: string, coveyTownID: string,
@@ -132,7 +160,7 @@ describe('TownsServiceAPIREST', () => {
         // Expected error
       }
     });
-    it('Deletes a town if given a valid password and town, no longer allowing it to be joined or listed', async () => {
+    it('Deletes a town and its channel if given a valid password and town, no longer allowing it to be joined or listed', async () => {
       const {coveyTownID, townUpdatePassword} = await createTownForTesting(undefined, true);
       await apiClient.deleteTown({
         coveyTownID,
@@ -151,6 +179,7 @@ describe('TownsServiceAPIREST', () => {
       if (listedTowns.towns.find(r => r.coveyTownID === coveyTownID)) {
         fail('Expected the deleted town to no longer be listed');
       }
+      expect(mockDeleteChannel).toHaveBeenCalled();
     });
   });
   describe('CoveyTownUpdateAPI', () => {
@@ -175,8 +204,20 @@ describe('TownsServiceAPIREST', () => {
       expectTownListMatches(await apiClient.listTowns(), pubTown1);
     });
     it('Updates the friendlyName and visbility as requested', async () => {
+
+      const mockSid = nanoid();
+      mockCreateChannel.mockReturnValue(Promise.resolve({
+        sid: mockSid,
+      }));
       const pubTown1 = await createTownForTesting(undefined, false);
       expectTownListMatches(await apiClient.listTowns(), pubTown1);
+      const mockResponse = {
+        sid: nanoid(),
+        friendlyName: 'newName',
+      };
+
+      mockUpdateChannel.mockReturnValue(mockResponse);
+
       await apiClient.updateTown({
         coveyTownID: pubTown1.coveyTownID,
         coveyTownPassword: pubTown1.townUpdatePassword,
@@ -186,10 +227,15 @@ describe('TownsServiceAPIREST', () => {
       pubTown1.friendlyName = 'newName';
       pubTown1.isPubliclyListed = true;
       expectTownListMatches(await apiClient.listTowns(), pubTown1);
+      expect(mockUpdateChannel).toHaveBeenCalledWith(mockSid, 'newName');
     });
     it('Does not update the visibility if visibility is undefined', async () => {
       const pubTown1 = await createTownForTesting(undefined, true);
       expectTownListMatches(await apiClient.listTowns(), pubTown1);
+      mockUpdateChannel.mockReturnValue({
+        sid: nanoid(),
+        friendlyName: 'newName2',
+      });
       await apiClient.updateTown({
         coveyTownID: pubTown1.coveyTownID,
         coveyTownPassword: pubTown1.townUpdatePassword,
@@ -216,6 +262,7 @@ describe('TownsServiceAPIREST', () => {
       }
     });
     it('Admits a user to a valid public or private town', async () => {
+      mockGetChatTokenForTown.mockReturnValue(Promise.resolve(nanoid()));
       const pubTown1 = await createTownForTesting(undefined, true);
       const privTown1 = await createTownForTesting(undefined, false);
       const res = await apiClient.joinTown({
@@ -238,8 +285,24 @@ describe('TownsServiceAPIREST', () => {
 
     });
   });
-  describe('CoveyTownChat', () => {
+  describe('CoveyTownChat Private Chat', () => {
+    let mockUniqueName: string;
+    beforeEach(() => {
+      mockCreateChannel.mockReset();
+      mockSendInvite.mockReset();
+      mockGetChatTokenForTown.mockReset();
+
+      mockUniqueName = nanoid();
+      mockCreateChannel.mockReturnValue(Promise.resolve({
+        sid: nanoid(),
+        uniqueName: mockUniqueName,
+      }));
+      mockGetChatTokenForTown.mockReturnValue(Promise.resolve(nanoid()));
+    });
     it('Should create a new private chat channel', async () => {
+      const mockResponse = {
+        uniqueName: mockUniqueName,
+      };
 
       const testTown = await createTownForTesting(undefined, true);
 
@@ -253,51 +316,179 @@ describe('TownsServiceAPIREST', () => {
         coveyTownID: testTown.coveyTownID,
       });
 
-      const mockClient = mock<TownsServiceClient>();
-
-      const mockUniqueName = nanoid();
-
-      const mockResponse = {
-        uniqueName: mockUniqueName,
-      };
-      mockClient.createPrivateChatChannel.mockReturnValue(Promise.resolve({
-        uniqueName: mockUniqueName,
-      }));
-
-      const response = await mockClient.createPrivateChatChannel({
+      const response = await apiClient.createPrivateChatChannel({
         currentPlayerID: res1.coveyUserID,
         coveyTownID: testTown.coveyTownID,
         otherPlayerID: res2.coveyUserID,
       });
 
+      // Create a channel for the public town and another for the private messaging channel
+      expect(mockCreateChannel).toBeCalledTimes(2);
       expect(response).toStrictEqual(mockResponse);
+    });
+    it('Should not create a new private chat channel if both playerIDs are empty', async () => {
+      const testTown = await createTownForTesting(undefined, true);
+
+      // Both playerIDs are empty strings
+      await expect(apiClient.createPrivateChatChannel({
+        currentPlayerID: '',
+        coveyTownID: testTown.coveyTownID,
+        otherPlayerID: '',
+      })).rejects.toThrow('Usernames cannot be empty');
+
+      // The current playerID is empty
+      await expect(apiClient.createPrivateChatChannel({
+        currentPlayerID: '',
+        coveyTownID: testTown.coveyTownID,
+        otherPlayerID: nanoid(),
+      })).rejects.toThrow('Usernames cannot be empty');
+
+      // The other playerID is empty
+      await expect(apiClient.createPrivateChatChannel({
+        currentPlayerID: nanoid(),
+        coveyTownID: testTown.coveyTownID,
+        otherPlayerID: '',
+      })).rejects.toThrow('Usernames cannot be empty');
+    });
+    it('Should not create a new private chat channel if both playerIDs are not in the same town', async () => {
+      const testTown = await createTownForTesting(undefined, true);
+
+      await expect(apiClient.createPrivateChatChannel({
+        currentPlayerID: nanoid(),
+        coveyTownID: testTown.coveyTownID,
+        otherPlayerID: nanoid(),
+      })).rejects.toThrow('Error processing request: Both players are not in the room');
+    });
+    it('Should not create a new private chat channel if there is already one existing between both players', async () => {
+
+      const testTown = await createTownForTesting(undefined, true);
+
+      const currentUserName = nanoid();
+      const otherUserName = nanoid();
+      const res1 = await apiClient.joinTown({
+        userName: currentUserName,
+        coveyTownID: testTown.coveyTownID,
+      });
+
+      const res2 = await apiClient.joinTown({
+        userName: otherUserName,
+        coveyTownID: testTown.coveyTownID,
+      });
+
+      const friendlyName = {
+        players: {
+          currentPlayer: {
+            playerID: res1.coveyUserID,
+            userName: currentUserName,
+          },
+          otherPlayer: {
+            playerID: res2.coveyUserID,
+            userName: otherUserName,
+          },
+        },
+      };
+
+      // Mock the return value for creating a private chat.
+      mockCreateChannel.mockReturnValueOnce(Promise.resolve({
+        sid: nanoid(),
+        friendlyName: JSON.stringify(friendlyName),
+        uniqueName: nanoid(),
+      }));
+
+      const firstResponse = await apiClient.createPrivateChatChannel({
+        currentPlayerID: res1.coveyUserID,
+        coveyTownID: testTown.coveyTownID,
+        otherPlayerID: res2.coveyUserID,
+      });
+
+      const secondResponse = await apiClient.createPrivateChatChannel({
+        currentPlayerID: res1.coveyUserID,
+        coveyTownID: testTown.coveyTownID,
+        otherPlayerID: res2.coveyUserID,
+      });
+
+      // The second response should just return the uniqueName of the already existing channel.
+      expect(firstResponse).toStrictEqual(secondResponse);
+
+      // Two channels created. One from creating the public chat and another for creating a single
+      // private chat channel.
+      expect(mockCreateChannel).toBeCalledTimes(2);
+
+      // Sends invites to both players when the channel is created and twice more if there is
+      // a duplicate
+      expect(mockSendInvite).toBeCalledTimes(4);
+
+    });
+  });
+  describe('CoveyTownChat Help Chat', () => {
+    let mockUniqueName: string;
+    beforeEach(() => {
+      mockCreateChannel.mockReset();
+      mockSendInvite.mockReset();
+      mockGetChatTokenForTown.mockReset();
+      mockCreateChannelWithBot.mockReset();
+
+      mockUniqueName = nanoid();
+      mockCreateChannel.mockReturnValue(Promise.resolve({
+        sid: nanoid(),
+        uniqueName: mockUniqueName,
+      }));
+      mockCreateChannelWithBot.mockReturnValue(Promise.resolve({
+        uniqueName: mockUniqueName,
+      }));
+
+      mockGetChatTokenForTown.mockReturnValue(Promise.resolve(nanoid()));
     });
     it('Should create a private chat channel with a bot', async () => {
 
-      const testTown = await createTownForTesting(undefined, true);
+      const mockResponse = {
+        uniqueName: mockUniqueName,
+      };
+
+      const testTown = await createTownForTesting(nanoid(), true);
 
       const res1 = await apiClient.joinTown({
         userName: nanoid(),
         coveyTownID: testTown.coveyTownID,
       });
 
-      const mockClient = mock<TownsServiceClient>();
-
-      const mockUniqueName = nanoid();
-
-      const mockResponse = {
-        uniqueName: mockUniqueName,
-      };
-      mockClient.createChatBotChannel.mockReturnValue(Promise.resolve({
-        uniqueName: mockUniqueName,
-      }));
-
-      const response = await mockClient.createChatBotChannel({
+      const response = await apiClient.createChatBotChannel({
         coveyTownID: testTown.coveyTownID,
         playerID: res1.coveyUserID,
       });
 
+      expect(mockSendInvite).toHaveBeenCalledTimes(1);
       expect(response).toStrictEqual(mockResponse);
+    });
+    it('Should should reuse an existing private chat channel with a bot', async () => {
+
+      const mockResponse = {
+        uniqueName: mockUniqueName,
+      };
+
+      const testTown = await createTownForTesting(nanoid(), true);
+
+      const res1 = await apiClient.joinTown({
+        userName: nanoid(),
+        coveyTownID: testTown.coveyTownID,
+      });
+
+      const firstResponse = await apiClient.createChatBotChannel({
+        coveyTownID: testTown.coveyTownID,
+        playerID: res1.coveyUserID,
+      });
+
+      const secondResponse = await apiClient.createChatBotChannel({
+        coveyTownID: testTown.coveyTownID,
+        playerID: res1.coveyUserID,
+      });
+
+      // The second response should just return the uniqueName of the already existing channel.
+      expect(firstResponse).toStrictEqual(secondResponse);
+
+      expect(mockSendInvite).toHaveBeenCalledTimes(2);
+
+      expect(mockCreateChannelWithBot).toHaveBeenCalledTimes(2);
     });
   });
 });
